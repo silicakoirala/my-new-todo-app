5cc0cf94cddd733ea0e152b08c1f212be04725d9
Silica Koirala <silicakoirala@gmail.com>
2024-04-26 12:39:39 +0545

    refactor(Ap.jsxp) clean up legacy code

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-
+        App
       </div>
     </>
   )
 }
 
-export default App
+export default App;
